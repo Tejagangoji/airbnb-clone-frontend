8cfa0d6f5981573eda73390f68242bf14eb28d25
teja <tejagangoji@gmail.com>
2024-07-14 18:52:25 +0530

    stylings framesworks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Route, Routes, Outlet } from 'react-router-dom';
 import Home from './pages/Home';
 import Context from './context/Context';
@@ -6,6 +7,10 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
 const Withnavandfooter = () => {
+  const [total, setitems] = [
+    { id: "", photos: ["kkdhsg", "shkdgfb", "dahf"] },
+    { id: 2, photos: ["kkdhsg", "shkdgfb", "dahf"] }
+  ]
   return (
     <>
       <Navbar />
