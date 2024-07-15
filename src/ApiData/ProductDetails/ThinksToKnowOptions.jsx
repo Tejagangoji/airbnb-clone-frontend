4d991af529671c289ec523fead9e0248eef89384
pentirajasekhar <pushpalathaoct17@gmail.com>
2024-07-15 09:55:01 +0530

    Created product details folder

---
+++ src/ApiData/ProductDetails/ThinksToKnowOptions.jsx
@@ -0,0 +1,38 @@
+export const thinksToKnowOptions = [
+    {
+        title: '',
+        options: [
+            {
+                name: 'House rules',
+            }
+            
+            
+        ],
+        hyperLinkName: 'Show more',
+        hyperLinkurl: ''
+    },
+    {
+        title: '',
+        options: [
+            {
+                name: 'House rules',
+            }
+            
+            
+        ],
+        hyperLinkName: 'Show more',
+        hyperLinkurl: ''
+    },
+    {
+        title: '',
+        options: [
+            {
+                name: 'House rules',
+            }
+            
+            
+        ],
+        hyperLinkName: 'Show more',
+        hyperLinkurl: ''
+    }
+]
