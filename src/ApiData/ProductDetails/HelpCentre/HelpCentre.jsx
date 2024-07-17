87b179c985f4962df446796b5d8a22a9ce9b5369
pentirajasekhar <pushpalathaoct17@gmail.com>
2024-07-17 18:51:41 +0530

    Help Centre Changes

---
+++ src/ApiData/ProductDetails/HelpCentre/HelpCentre.jsx
@@ -0,0 +1,92 @@
+import IMG1 from "../../../assets/HelpCentre/Guests/1.jpg";
+import IMG2 from "../../../assets/HelpCentre/Guests/2.jpg";
+import IMG3 from "../../../assets/HelpCentre/Guests/3.jpg";
+import IMG4 from "../../../assets/HelpCentre/Guests/4.jpg";
+import IMG5 from "../../../assets/HelpCentre/Guests/5.jpg";
+import IMG6 from "../../../assets/HelpCentre/Guests/6.jpg";
+import IMG7 from "../../../assets/HelpCentre/Guests/7.jpg";
+import IMG8 from "../../../assets/HelpCentre/Guests/8.jpg";
+
+export const GuestImages = [
+  {
+    img: IMG1,
+    title: "Getting started with Airbnb",
+  },
+  {
+    img: IMG2,
+    title: "Access and manage your account",
+  },
+  {
+    img: IMG3,
+    title: "Help with a reservation",
+  },
+  {
+    img: IMG4,
+    title: "AirCover for guests",
+  },
+];
+
+export const HostImages = [
+  {
+    img: IMG1,
+    title: "Getting started with Airbnb",
+  },
+  {
+    img: IMG5,
+    title: "Help with a reservation",
+  },
+  {
+    img: IMG3,
+    title: "AirCover for guests",
+  },
+  {
+    img: IMG4,
+    title: "Access and manage your account",
+  },
+];
+
+export const ExperienceHostImages = [
+  "../../../assets/HelpCentre/Guests/1.jpg",
+  "../../../assets/HelpCentre/Guests/5.jpg",
+  "../../../assets/HelpCentre/Guests/3.jpg",
+  "../../../assets/HelpCentre/Guests/7.jpg",
+];
+export const TravelImages = [
+  "../../../assets/HelpCentre/Guests/1.jpg",
+  "../../../assets/HelpCentre/Guests/5.jpg",
+  "../../../assets/HelpCentre/Guests/8.jpg",
+  "../../../assets/HelpCentre/Guests/2.jpg",
+];
+
+export const ArticlesData = [
+  {
+    title: "Cancelling your reservation for a stay",
+    link: "",
+    para: "Your plans have changed and now you need to cancel your reservation. No problem.",
+  },
+  {
+    title: "Payment methods accepted",
+    link: "",
+    para: "Airbnb supports different payment methods, depending on the country your payment...",
+  },
+  {
+    title: "Change the date or time of your Experience reservation",
+    link: "",
+    para: "You booked an Experience, but the date or time no longer works for you? Don’t worry, you",
+  },
+  {
+    title: "Editing, removing, or adding a payment method",
+    link: "",
+    para: "If an existing payment method on your account is incorrect (e.g. an expired credit...",
+  },
+  {
+    title: "If your Host cancels your reservation",
+    link: "",
+    para: "While it’s rare, sometimes a Host may need to cancel a reservation. We understand this can...",
+  },
+  {
+    title: "When you’ll pay for your reservation",
+    link: "",
+    para: "You just made a reservation – congrats! So, what happens next? Your payment method will",
+  },
+];
--- src/components/HelpCentre/HelpCentre.jsx
+++ src/components/HelpCentre/HelpCentre.jsx
@@ -1,5 +1,10 @@
+import {
+  ArticlesData,
+  GuestImages,
+} from "../../ApiData/ProductDetails/HelpCentre/HelpCentre";
 import "./HelpCentre.css";
 import { RiSearchLine as SearchIcon } from "react-icons/ri";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 
 export function Header() {
   const handleSubmit = (e) => {
@@ -22,7 +27,90 @@ export function Header() {
   );
 }
 export function Filtering() {
-  return <div></div>;
+  return (
+    <div>
+      <span className="FilterSpan">
+        <button> Guest</button>
+        <button>Host</button>
+        <button>Experience Host</button>
+        <button>Travel admin</button>
+      </span>
+      <hr />
+    </div>
+  );
+}
+export function UserInfo() {
+  return (
+    <div className="Block">
+      <span className="block-container">
+        <div className="block1">
+          <h5>We're here for you</h5>
+          <p>Log in to get help with your reservations, account, and more.</p>
+        </div>
+        <div className="block2">
+          <button type="submit">Log in or sign up</button>
+        </div>
+      </span>
+    </div>
+  );
+}
+export function BrowseInfo() {
+  return (
+    <div className="browserdiv">
+      <div className="browserguiding">
+        <h3 className="Browsing">Guides for getting started</h3>
+      </div>
+      <div>
+        <a className="BrowseArrow" href="Browse all Topics">
+          Browse all Topics
+          <MdOutlineKeyboardArrowRight />
+        </a>
+      </div>
+    </div>
+  );
+}
+export function ImageCard() {
+  return (
+    <div style={{ display: "flex" }}>
+      {GuestImages?.map((item, index) => {
+        console.log(item);
+        return (
+          <div className="imagesContainer">
+            <img
+              className="images"
+              src={item.img}
+              alt="Hello"
+              width="90%"
+              height="100%"
+            />
+            <h6>{item.title}</h6>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+export function Articles() {
+  return (
+    <div>
+      <h4 style={{ margin: "20px" }}>Top Articles</h4>
+      <div className="articlesContainer">
+        {ArticlesData?.map((item, index) => {
+          return (
+            <div>
+              <div className="articleGrid">
+                <a className="articleLink" href={item.link}>
+                  {item.title}{" "}
+                </a>
+                <div style={{ marginLeft: "5px" }}>{item.para}</div>
+                <hr style={{ margin: "20px" }} />
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default function HelpCentre() {
@@ -31,6 +119,10 @@ export default function HelpCentre() {
       <div className="helpCentreWrap">
         <Header />
         <Filtering />
+        <UserInfo />
+        <BrowseInfo />
+        <ImageCard />
+        <Articles />
       </div>
     </div>
   );
